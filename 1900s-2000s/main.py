575739fce4454125c5d523ccce7d7ecac7bf1c50
CVXSL <70546159+CVXSL@users.noreply.github.com>
2022-01-30 23:36:34 -0700

    Added Lofi Music

---
+++ 1900s-2000s/main.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned,
+    help_command=None, description="Nalo Music Bot",
+    activity=discord.Game("Loading..."),
+    case_insensitive=True,
+#    owner_id=355121277046095872,
+    owner_id=852572302590607361,
+    intents=discord.Intents.all()
+)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game("Music"))
+    bot.vc = await bot.get_channel(937540353878261810).connect()
+#    bot.vc.play(discord.FFmpegOpusAudio('song2.mp3'))
+    print("Ready!")
+
+    channel = bot.get_channel(937586830914756639)
+
+    while True:
+            bot.vc.play(discord.FFmpegOpusAudio('Promiscuous.mp3'))
+            await channel.send("**🔥┃Now Playing Promiscuous!**")
+            await asyncio.sleep(245)
+            bot.vc.play(discord.FFmpegOpusAudio('Last Friday Night.mp3'))
+            await channel.send("**🔥┃Now Playing Last Friday Night!**")
+            await asyncio.sleep(234)
+            bot.vc.play(discord.FFmpegOpusAudio('Give Me Everything.mp3'))
+            await channel.send("**🔥┃Now Playing Give Me Everything!**")
+            await asyncio.sleep(249)
+            bot.vc.play(discord.FFmpegOpusAudio('Toxic.mp3'))
+            await channel.send("**🔥┃Now Playing Toxic!**")
+            await asyncio.sleep(203)
+
+bot.run(os.getenv("token"))
